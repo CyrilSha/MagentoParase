c52c64da1be4e8c569cc93f9ac4e614034cf56af
CyrilSha <samuelcoolmail@gmail.com>
2015-11-10 10:36:28 -0500

    Create MagentoParse.py

---
+++ MagentoParse.py
@@ -0,0 +1,17 @@
+import urllib2
+
+#DomainList.txt should looks like http://domain.com/admin
+fileDomain = open("DomainList.txt")
+fileMagento = open("DiscoveredMagento.txt","w")
+
+for line in file:
+    try:
+        response = urllib2.urlopen(line[:-1])
+        html = response.read()
+        BufStr = str(html)
+        BufSer = BufStr.find("Magento")
+        if BufSer != -1:
+            fileMagento.write(line+"\n")
+            print line
+    except:
+         pass
